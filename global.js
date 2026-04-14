22cb87cf9c10175dbc3657e08a3671acd16efcf9
Matthew Viray <matvir437@gmail.com>
2026-04-14 15:01:20 -0700

    Step 1-3 Lab 3
    Step 1: Add JS file to all pages,  Step 2: Automatic current page link, Step 3: Automatic navigation menu

---
+++ global.js
@@ -0,0 +1,51 @@
+console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// const navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/matthewviray', title: 'GitHub' },
+  { url: 'Resume/', title: 'Resume' }
+];
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a)
+
+  a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+  );
+  
+  a.toggleAttribute('target', a.host !== location.host);
+}
+
+
